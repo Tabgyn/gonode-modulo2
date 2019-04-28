7ca47ba4755eb9654ebb99b099244264f7a560c2
Tiago Azevedo Borges <tabgyn@gmail.com>
2019-04-28 15:42:44 -0300

    retirado os console.log

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -5,8 +5,7 @@ const { Appointment } = require('../models')
 class AvailableController {
   async index (req, res) {
     const date = moment(parseInt(req.query.date))
-    console.log(date.startOf('day').format())
-    console.log(date.endOf('day').format())
+
     const appointments = await Appointment.findAll({
       where: {
         provider_id: req.params.provider,
